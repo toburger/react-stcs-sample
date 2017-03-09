6ffddf3a4e9a7a6a233baab4e591ed2a00a78c4b
Tobias Burger <tobias_burger@hotmail.com>
2017-03-09 16:40:45 +0100

    add random gif sample

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import { simpleApp } from './Api'
 import * as Counter from './Counter'
 import * as CounterPair from './CounterPair'
 import * as CounterList from './CounterList'
+import * as RandomGif from './RandomGif'
+import { loop, Effects } from 'redux-loop'
 
-const init = {
+const [randomGifModel, randomGifEffect] = RandomGif.init('cat')
+
+const init = loop({
   counter: Counter.init(0),
   counterPair: CounterPair.init(0, 0),
-  counterList: CounterList.init([0, 0, 0])
-}
+  counterList: CounterList.init([0, 0, 0]),
+  randomGif: randomGifModel
+}, Effects.batch([randomGifEffect]))
 
 const reducer = {
   counter: Counter.reducer,
   counterPair: CounterPair.reducer,
-  counterList: CounterList.reducer
+  counterList: CounterList.reducer,
+  randomGif: RandomGif.reducer
 }
 
 const view = () =>
@@ -23,6 +29,8 @@ const view = () =>
     <CounterPair.connectedView />
     <hr />
     <CounterList.connectedView />
+    <hr />
+    <RandomGif.connectedView />
   </div>
 
 const App = simpleApp(init, reducer, view)
---
+++ src/RandomGif.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import {loop, Effects} from 'redux-loop'
+import * as Api from './Api'
+import R from 'ramda'
+import superagent from 'superagent'
+import Promise from 'bluebird'
+
+const REQUEST_MORE = 'REQUEST_MORE'
+const REQUEST_ERROR = 'REQUEST_ERROR'
+const NEW_GIF = 'NEW_GIF'
+
+const request = Promise.promisifyAll(superagent)
+
+export const requestMore = () => ({
+    type: REQUEST_MORE
+})
+
+export const requestError = error => ({
+    type: REQUEST_ERROR,
+    error
+})
+
+export const newGif = url => ({
+    type: NEW_GIF,
+    url
+})
+
+const fetchRandomGif = topic =>
+    request.get('http://api.giphy.com/v1/gifs/random')
+        .query({
+            api_key: 'dc6zaTOxFJmzC',
+            tag: topic
+        })
+        .endAsync()
+        .then(res => {
+            const url = R.path(['body', 'data', 'image_url'], res)
+            return newGif(url)
+        })
+        .catch(requestError)
+
+export const init = topic =>
+    loop({
+            topic,
+            gifUrl: ''
+        },
+        Effects.promise(fetchRandomGif, topic))
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case REQUEST_MORE:
+            return loop(state, Effects.promise(fetchRandomGif, state.topic))
+        case REQUEST_ERROR:
+            return loop(state, Effects.none())
+        case NEW_GIF:
+            return loop({
+                ...state,
+                gifUrl: action.url
+            }, Effects.none())
+        default:
+            return loop(state, Effects.none())
+    }
+}
+
+export const view = ({ model, dispatch }) =>
+    <div>
+        <img
+            src={R.prop('gifUrl', model)}
+            style={{widht: '200px'}} />
+        <button
+            onClick={() => dispatch(requestMore())}>More</button>
+    </div>
+
+export const connectedView = Api.connectView(state => state.randomGif, view)
