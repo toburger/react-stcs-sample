32021d968b327e724b2dd1c41ff836640847f70d
Tobias Burger <tobias_burger@hotmail.com>
2017-03-10 16:51:59 +0100

    remove Api calls from helloworld example

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,12 +1,7 @@
 import React from 'react'
-import * as Api from './Api'
 
-export const init = () => []
+export const init = () => {}
 
 export const reducer = (state, action) => state
 
-export const view = () =>
-    <div>hello world</div>
-
-export const connectedView =
-    Api.connectView(state => state.helloWorld, view)
+export const view = () => <div>hello world</div>
